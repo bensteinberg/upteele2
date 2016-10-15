4841c91318cbfa88ce5f103b8a25e2234b8af384
Ben Steinberg <benjamin_steinberg@harvard.edu>
2016-10-15 10:26:45 -0400

    Reindent so as to stop fighting it, add plugin for production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
+const webpack = require('webpack');
 var config = {
-  context: __dirname + "/app",
-  entry: "./main.js",
+    context: __dirname + "/app",
+    entry: "./main.js",
 
-  output: {
-    filename: "bundle.js",
-    path: __dirname + "/dist",
-  },
-  module: {
-      loaders: [
-          { test: /\.css$/, loader: "style-loader!css-loader" },
-          {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              loader: 'babel-loader',
-              query: {
-                  presets: ['react', 'es2015']
-              }
-          }
-      ],
-  },
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist",
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ],
 };
 module.exports = config;
